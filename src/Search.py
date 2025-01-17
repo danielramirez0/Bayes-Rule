358a9e1b21fdef1a77d44839dff8388ec30cfd2c
Daniel Ramirez <zoerunner@gmail.com>
2025-01-17 13:06:09 -0800

    Add Search class with init

---
+++ src/Search.py
@@ -0,0 +1,33 @@
+import sys
+import cv2 as cv
+from .main import MAP_FILE
+from .main import SA1_CORNERS, SA2_CORNERS, SA3_CORNERS
+
+
+class Search():
+    """Bayesian Search & Rescue app with 3 search areas."""
+
+    def __init__(self, name):
+        self.name = name
+        self.img = cv.read(MAP_FILE, cv.IMREAD_COLOR)
+        if self.img is None:
+            print(f"Could not open or find file {MAP_FILE}", file=sys.stderr)
+            # raise FileNotFoundError(f"Could not open or find file {MAP_FILE}")
+            sys.exit(1)
+
+        self.area_actual = 0
+        self.sailor_actual = (0, 0) # As "local" coords within search area
+
+        self.sa1 = self.img[SA1_CORNERS[1]:SA1_CORNERS[3], SA1_CORNERS[0]:SA1_CORNERS[2]]
+        
+        self.sa2 = self.img[SA2_CORNERS[1]:SA2_CORNERS[3], SA2_CORNERS[0]:SA2_CORNERS[2]]
+
+        self.sa3 = self.img[SA3_CORNERS[1]:SA3_CORNERS[3], SA3_CORNERS[0]:SA3_CORNERS[2]]
+
+        self.p1 = 0.2
+        self.p2 = 0.5
+        self.p3 = 0.3
+
+        self.sep1 = 0
+        self.sep2 = 0
+        self.sep3 = 0

7286628902e836d4a6817bb5e049edb7295e0ab2
Daniel Ramirez <zoerunner@gmail.com>
2025-01-21 18:09:42 -0800

    Add main func, draw func, and clean imports

--- src/main.py
+++ src/main.py
@@ -1,11 +1,91 @@
 import sys
-import random
-import itertools
-import numpy as np
-import cv2 as cv
+import search
+#import os
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 MAP_FILE = 'img/cape_python.png'
 
 SA1_CORNERS = (130, 265, 180, 315) # (UL-X, UL-Y, LR-X, LR-Y)
 SA2_CORNERS = (80, 255, 130, 305) # (UL-X, UL-Y, LR-X, LR-Y)
 SA3_CORNERS = (105, 205, 155, 255) # (UL-X, UL-Y, LR-X, LR-Y)
+
+def draw_menu(search_num):
+    """Print menu of choices for conducting area searches."""
+    print('\nSearch {}'.format(search_num))
+    print(
+        """
+        Choose next areas to search:
+
+        0 - Quit
+        1 - Search Area 1 twice
+        2 - Search Area 2 twice
+        3 - Search Area 3 twice
+        4 - Search Areas 1 & 2
+        5 - Search Areas 1 & 3
+        6 - Search Areas 2 & 3
+        7 - Start Over
+        """
+    )
+
+def main():
+    app = search.Search('Cape_Python')
+    app.draw_map(last_known=(160,290))
+    sailor_x, sailor_y = app.sailor_final_location(num_search_areas=3)
+    print("-" * 65)
+    print("\nInitial Target (P) Probabilities:")
+    print("P1 = {:.3f}, P2 = {:.3f}, P3 = {:.3f}".format(app.p1, app.p2, app.p3))
+    search_num = 1
+
+    while True:
+        app.calc_search_effectiveness()
+        draw_menu(search_num)
+        choice = input("Choice: ")
+
+        if choice == '0':
+            sys.exit()
+
+        elif choice == '1':
+            results_1, coords_1 = app.conduct_search(1, app.sa1, app.sep1)
+            results_2, coords_2 = app.conduct_search(1, app.sa1, app.sep1)
+            app.sep1 = (len(set(coords_1 + coords_2))) / (len(app.sa1)**2)
+            app.sep2 = 0
+            app.sep3 = 0
+
+        elif choice == '2':
+            results_1, coords_1 = app.conduct_search(2, app.sa2, app.sep2)
+            results_2, coords_2 = app.conduct_search(2, app.sa2, app.sep2)
+            app.sep1 = 0
+            app.sep2 = (len(set(coords_1 + coords_2))) / (len(app.sa2)**2)
+            app.sep3 = 0
+
+        elif choice == '3':
+            results_1, coords_1 = app.conduct_search(3, app.sa3, app.sep3)
+            results_2, coords_2 = app.conduct_search(3, app.sa3, app.sep3)
+            app.sep1 = 0
+            app.sep2 = 0
+            app.sep3 = (len(set(coords_1 + coords_2))) / (len(app.sa3)**2)
+
+        elif choice == '4':
+            results_1, coords_1 = app.conduct_search(1, app.sa1, app.sep1)
+            results_2, coords_2 = app.conduct_search(2, app.sa2, app.sep2)
+            app.sep3 = 0
+
+        elif choice == '5':
+            results_1, coords_1 = app.conduct_search(1, app.sa1, app.sep1)
+            app.sep2 = 0
+            results_2, coords_2 = app.conduct_search(3, app.sa3, app.sep3)
+
+        elif choice == '6':
+            app.sep1 = 0
+            results_1, coords_1 = app.conduct_search(2, app.sa2, app.sep2)
+            results_2, coords_2 = app.conduct_search(3, app.sa3, app.sep3)
+
+        elif choice == "7":
+            main()
+
+        else:
+            print("\nSorry, but that isn't a valid choice.", file=sys.stderr)
+            continue
+
+if __name__ == "__main__":
+    main()
